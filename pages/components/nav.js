e2236e5f36c08a3b7287ea653bd67d667989dcbd
Swetha-Seenivas <swetha.seenivas@anywhere.co>
2023-11-30 21:36:35 +0530

    change for setmore hero section with tailwind

---
+++ pages/components/nav.js
@@ -0,0 +1,11 @@
+export default function Nav() {
+  return (
+    <div className="m-5 flex gap-10">
+      <img
+        className="mr-auto"
+        src="https://assets.setmore.com/website/next/images/common/setmore-logo.svg"
+      />
+      <p className="text-slate-500">Login</p>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        "setmore-blue": "#1d90f5",
+        "hover-blue": "#126ab8",
+      },
+      fontFamily: {
+        euclid: ["Euclid Circular B", "Arial", "Helvetica", "sans-serif"],
+        lato: ["Lato", "Arial", "Helvetica", "sans-serif"],
+        inter: ["Inter", "Arial", "Helvetica", "sans-serif"],
       },
     },
   },
   plugins: [],
-}
+};
